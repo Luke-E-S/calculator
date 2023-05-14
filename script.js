080a57faed0a474394f8cff7fea4c9fac76eafdd
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-14 19:19:59 +0100

    feat: button events to add numbers to display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let [firstNumber, secondNumber, operator] = [0, 0, ""];
+let [firstNumber, secondNumber, operator] = ["", "", ""];
 
 function add(a, b) {
     return a+b;
@@ -27,3 +27,65 @@ function operate(n1, n2, op) {
         return divide(n1, n2);
     }
 }
+const display = document.querySelector("#display");
+const btnOne = document.getElementById("one");
+const btnTwo = document.getElementById("two");
+const btnThree = document.getElementById("three");
+const btnFour = document.getElementById("four");
+const btnFive = document.getElementById("five");
+const btnSix = document.getElementById("six");
+const btnSeven = document.getElementById("seven");
+const btnEight = document.getElementById("eight");
+const btnNine = document.getElementById("nine");
+const btnZero = document.getElementById("zero");
+
+btnOne.addEventListener("click", () => {
+    firstNumber += "1";
+    display.textContent = firstNumber;
+    }
+);
+btnTwo.addEventListener("click", () => {
+    firstNumber += "2";
+    display.textContent = firstNumber;
+    }
+);
+btnThree.addEventListener("click", () => {
+    firstNumber += "3";
+    display.textContent = firstNumber;
+    }
+);
+btnFour.addEventListener("click", () => {
+    firstNumber += "4";
+    display.textContent = firstNumber;
+    }
+);
+btnFive.addEventListener("click", () => {
+    firstNumber += "5";
+    display.textContent = firstNumber;
+    }
+);
+btnSix.addEventListener("click", () => {
+    firstNumber += "6";
+    display.textContent = firstNumber;
+    }
+);
+btnSeven.addEventListener("click", () => {
+    firstNumber += "7";
+    display.textContent = firstNumber;
+    }
+);
+btnEight.addEventListener("click", () => {
+    firstNumber += "8";
+    display.textContent = firstNumber;
+    }
+);
+btnNine.addEventListener("click", () => {
+    firstNumber += "9";
+    display.textContent = firstNumber;
+    }
+);
+btnZero.addEventListener("click", () => {
+    firstNumber += "0";
+    display.textContent = firstNumber;
+    }
+);
