beaa3222166ddea9c26506555f1a0634e9907428
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-14 10:52:58 +0100

    feat: added calculator ui features

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+function operate(n1, n2, op) {
+    if(op === "+") {
+        return add(n1, n2);
+    }
+    else if(op === "-") {
+        return subtract(n1, n2);
+    }
+    else if(op === "*") {
+        return multiply(n1, n2);
+    }
+    else if(op === "/") {
+        return divide(n1, n2);
+    }
+}
